1ed195496e050af47c3a4fc2639cce2aa5446a61
Enless <fghjk455rg@mail.ru>
2024-01-17 06:28:09 +0700

    Add screwdriver and heavy conveyor

--- src/content/KatorBlocks.java
+++ src/content/KatorBlocks.java
@@ -2,9 +2,12 @@ package content;
 
 import arc.graphics.Color;
 import mindustry.content.Fx;
+import mindustry.content.Liquids;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.world.Block;
+import mindustry.world.blocks.distribution.Conveyor;
+import mindustry.world.blocks.production.Drill;
 import mindustry.world.blocks.production.GenericCrafter;
 import mindustry.world.blocks.storage.CoreBlock;
 import mindustry.world.draw.DrawGlowRegion;
@@ -17,18 +20,39 @@ import static mindustry.type.ItemStack.with;
 
 public class KatorBlocks {
     public static Block
-            //crafter
+            //production
+            screwdriver,
+            //distribution
+            heavyconveyor,
+            //crafting
             erelitforge,
             //storage
             corestardust
             ;
 
     public static void load() {
+        //production
+        screwdriver = new Drill("screwdriver"){{
+            requirements(Category.production, with(KatorItems.kateos, 10, KatorItems.nikel, 18));
+            tier = 2;
+            drillTime = 300;
+            size = 2;
+            //mechanical drill doesn't work in space
+            envEnabled ^= Env.space;
+            consumeLiquid(Liquids.water, 0.05f).boost();
+        }};
+        //distribution
+        heavyconveyor = new Conveyor("heavy-conveyor"){{
+                requirements(Category.distribution, with(KatorItems.kateos, 2, KatorItems.nikel, 1));
+                health = 100;
+                speed = 0.03f;
+                displayedSpeed = 4.2f;
+        }};
+        //crafting
         erelitforge = new GenericCrafter("erelit-forge"){{
             requirements(Category.crafting, with(KatorItems.ferelit, 60, KatorItems.kateos, 60, KatorItems.nikel, 80));
             size = 3;
-            craftTime = 300;
-            solid = true;
+            craftTime = 240;
             drawer = new DrawMulti(
                     new DrawRegion("-bottom"),
                     new DrawPlasma(){{
@@ -43,23 +67,23 @@ public class KatorBlocks {
                         glowScale = 5f;
                     }}
             );
-            craftEffect = Fx.smokeCloud;
-            outputItems = ItemStack.with(KatorItems.erelit, 2);
-            consumeItems(with(KatorItems.ferelit, 1, KatorItems.nikel, 1,  KatorItems.sulfur, 2));
+            craftEffect = Fx.blastsmoke;
+            consumeItems(with(KatorItems.ferelit, 1, KatorItems.nikel, 1,  KatorItems.calcite, 2));
             consumePower(3f);
+            outputItems = ItemStack.with(KatorItems.erelit, 2);
             squareSprite = false;
         }};
+        //storage
         corestardust = new CoreBlock("core-star-dust"){{
             requirements(Category.effect, with( KatorItems.nikel, 400, KatorItems.ferelit, 600, KatorItems.kateos, 800));
             size = 3;
             alwaysUnlocked = true;
-            squareSprite = false;
             isFirstTier = true;
             //unitType = ;
             health = 2000;
             itemCapacity = 2000;
             unitCapModifier = 10;
-            envEnabled ^= Env.space;
+            squareSprite = false;
         }};
     }
 }
